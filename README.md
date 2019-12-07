89971dea7695117e5dae9060d866772d4bdb324d
Bruce Anderson <anderson.bruce.b@gmail.com>
2019-12-06 19:10:37 -0500

    ...

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ does the following:
 - [ ] Clones the template into the location specified by target string 
 - [ ] If no target string, just clone and append to parent (?) 
 - [ ] Changing be-born back to false / true doesn't do anything anymore. 
-- [ ]  Holds on to reference of cloned element.  cloned element also gets reference to open-border birthplace 
+- [ ] Holds on to reference of cloned element.  cloned element also gets reference to open-border birthplace 
 - [ ] Dynamically attaches properties attributes to open-borders tag, based on schema prop (follows custom-elements.json schema). 
 - [ ] If addEventListener added to open-borders, actually attaches event listener to remote reference (my-element).
 - [ ] If target changes, moves element to new target.
--- open-borders.ts
+++ open-borders.ts
@@ -1,3 +1,41 @@
-export class OpenBorders extends HTMLElement{
+import {XtallatX} from 'xtal-element/xtal-latx.js';
+import {hydrate} from 'trans-render/hydrate.js';
 
+const target = 'target';
+const be_born = 'be-born';
+
+export class OpenBorders extends XtallatX(hydrate(HTMLElement)){
+    static get observedAttributes(){
+        return super.observedAttributes.concat([target]);
+    }
+    attributeChangedCallback(n: string, ov: string, nv: string){
+        switch(n){
+            case target:
+                this._target = nv;
+                break;
+            case be_born:
+                this._be_born = nv !== null;
+        }
+        this.onPropsChange();
+    }
+    _target: string;
+    get target(){
+        return this._target;
+    }
+    set target(nv){
+        this.attr(target, nv);
+    }
+    _be_born: boolean;
+    get beBorn(){
+        return this._be_born;
+    }
+    _c: boolean;
+    connectedCallback(){
+        this._c = true;
+        this.onPropsChange();
+    }
+    onPropsChange(){
+        if(!this._c || this._disabled || !this._be_born) return;
+        
+    }
 }
